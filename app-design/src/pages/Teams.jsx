1602737edfd92244fe029adb908b7d9b409c17fe
WEB5LAB <88079157+web5lab@users.noreply.github.com>
2025-08-29 06:59:25 +0530

    Remove duplicate JSX comment

--- app-design/src/pages/Teams.jsx
+++ app-design/src/pages/Teams.jsx
@@ -200,7 +200,6 @@ export function Teams() {
         </div>
       </div>
       {/* Invite Member Modal */}
-      {/* Invite Member Modal */}
       {showInviteModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
           <div className="bg-white dark:bg-gray-800 rounded-lg shadow-xl w-full max-w-md">

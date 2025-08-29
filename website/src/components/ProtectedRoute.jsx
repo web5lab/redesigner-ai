e3e5122917c60a08c659edbe53876132ca0242e7
WEB5LAB <88079157+web5lab@users.noreply.github.com>
2025-08-29 14:32:14 +0530

    Fix routing redirect to landing page issue

--- website/src/components/ProtectedRoute.jsx
+++ website/src/components/ProtectedRoute.jsx
@@ -1,20 +1,20 @@
 import { Navigate, useLocation, Outlet } from 'react-router-dom';
 import { useBot } from '../contexts/BotContext';
 import { useSelector } from 'react-redux';
-import { logedInSelector, userSelector } from '../store/global.Selctor';
+import { logedInSelector, userSelector, activeBotSelector } from '../store/global.Selctor';
 
 export function ProtectedRoute({ children, requiresBot = false }) {
   const logedIn = useSelector(logedInSelector);
   const user = useSelector(userSelector);
-  const { selectedBot } = useBot();
+  const activeBot = useSelector(activeBotSelector);
   const location = useLocation();
 
   if (!logedIn || !user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
-  if (requiresBot && !selectedBot) {
-    return <Navigate to="/" replace />;
+  if (requiresBot && !activeBot) {
+    return <Navigate to="/dashboard" replace />;
   }
 
   return children;

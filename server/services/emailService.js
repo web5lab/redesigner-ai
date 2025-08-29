14d7940cd203efe887971254c9043209b5ac4798
WEB5LAB <88079157+web5lab@users.noreply.github.com>
2025-08-29 21:06:11 +0530

    Updated emailService.js

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Configure email transporter
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST || "smtp.gmail.com",
     port: process.env.EMAIL_PORT || 587,
     secure: false, // true for 465, false for other ports

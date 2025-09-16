2642f7188056a07d68c82959c065c19668abe9be
WEB5LAB <88079157+web5lab@users.noreply.github.com>
2025-09-17 04:13:35 +0530

    Deleted App.jsx

--- project/src/consoleiq.js
+++
@@ -1,8 +0,0 @@
-import { createConsoleIQ } from 'consoleiq';
-
-createConsoleIQ({
-    apiKey: 'console_1fd7507b3d38477f9e3183eb7f66306e',
-    name: 'redseignr-frontend',
-    allowedLevels: ["error",],
-    environment: 'browser'
-  });
